003d56775b6b82f3db3f5c568d86dfc8c925ea19
unknown <Leroydsouza544@gmail.com>
2020-02-06 10:11:02 +0300

    Python Strings

---
+++ python strings.py
@@ -0,0 +1,15 @@
+#https://github.com/LEROY482/Python-Strings.git
+#Python Strings Classwork
+a= "Hello world!"
+print (a)
+print (a[0])
+print (a[5])
+print (a[4])
+print (a[-1])
+print (a[:])
+print (a[2:5])
+print ("d"[1:])
+print (a[:8])
+print (a[::])
+print (a[-3])
+#Each string of code helps to say Hello world at the end
