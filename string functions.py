09be68b80fc02cd74efbfb80bb6914ccbc959196
LEROY482 <leroydsouza544@gmail.com>
2020-02-06 10:21:21 +0300

    string functions.py

---
+++ string functions.py
@@ -0,0 +1,12 @@
+a="hello world"
+n= "HELLO"
+b= a.capitalize()
+print (b)
+print (a.capitalize()) #Makes the first letter in Capital letters
+print (a.upper()) #Makes all the letters in capital letters
+print (n.lower()) #Makes all letters in small letters
+print (a.replace("H","J")) #replaces one string with another
+print (a.split(",")) #splits the strings into substrings
+print (a.strip()) #Removes any whitespace from beginning to end
+print (a.encode()) #Encodes the string
+print (a.endswith("d")) #Returns TRUE if the strings end with the specified value
