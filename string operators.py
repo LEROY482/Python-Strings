f74eaafbe1c68eddcb04982de478ea3ced84c397
LEROY482 <leroydsouza544@gmail.com>
2020-02-06 10:18:24 +0300

    Python Strings

---
+++ string operators.py
@@ -0,0 +1,10 @@
+a ="hello"
+b ="world"
+print (a+b)
+print (a*4)
+print (r"c:\documents\python")
+print ("hi {}".format (b))
+print ('h' in a)
+print ('n' not in a)
+#using string operators method concatenation
+#string operators include +, *, r, %, .format (), in, not in
